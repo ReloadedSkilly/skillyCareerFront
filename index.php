885c16e722fde04dcb02ef6d6a07e8cada491e16
Vincent Zerbib <vincentzerbib@gmail.com>
2018-07-27 11:49:04 +0200

    intro page

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
+
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array());
 
-echo $twig->render('index.html', array(
-      'userFirstname' =>'Jean',
-      'userLastname'=>'Veuplusse',
-      'userJob'=>'Compagnon sur site nucléaire',
-));
+echo $twig->render('index.html', array());
 
 ?>
